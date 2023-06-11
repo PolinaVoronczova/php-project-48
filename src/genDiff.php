a8b9570e5f3f210d5abdbc19348cb9066dff68d4
VoronczovaPolina <tirabyrius@gmail.com>
2023-06-11 14:12:56 +0500

    change genDiff structure

--- src/genDiff.php
+++ src/genDiff.php
@@ -6,32 +6,33 @@ function genDiff($pathFile1, $pathFile2)
 {
     var_dump($pathFile1);
     var_dump($pathFile2);
-    $file1 = json_decode(file_get_contents($pathFile1),true);
+    $file1 = json_decode(file_get_contents($pathFile1), true);
     ksort($file1);
     var_dump($file1);
-    $file2 = json_decode(file_get_contents($pathFile2),true);
+    $file2 = json_decode(file_get_contents($pathFile2), true);
     ksort($file2);
     var_dump($file2);
     $difference = '';
 
-    array_map(function ($key, $value) use (&$file2, &$difference) {
+    array_walk($file1, function ($value, $key) use (&$file2, &$difference) {
         if (array_key_exists($key, $file2)) {
             if ($value !== $file2[$key]) {
-                $difference .= '- ' . $key . ': ' . (string)$value . "\n";
-                $difference .= '+ ' . $key . ': ' . (string)$file2[$key] . "\n";
+                $difference .= '- ' . $key . ': ' . var_export($value, true) . "\n";
+                $difference .= '+ ' . $key . ': ' . var_export($file2[$key], true) . "\n";
                 $file2[$key] = null;
             } else {
-                $difference .= $key . ': ' . (string)$value . "\n";
+                $difference .= $key . ': ' . var_export($value, true) . "\n";
                 $file2[$key] = null;
             }
         } else {
-            $difference .= '- ' . $key . ': ' . (string)$value . "\n";
+            $difference .= '- ' . $key . ': ' . var_export($value, true) . "\n";
         }
-    }, array_keys($file1), $file1);
+    }
+    );
     $newItems = array_filter($file2, fn($n) => !is_null($n));
-    array_map(function ($key, $value) use (&$difference) {
-            $difference .= '+ ' . $key . ': ' . (string)$value . "\n";
-        }
-    , array_keys($newItems), $newItems);
+    array_walk($newItems, function ($value, $key) use (&$difference) {
+        $difference .= '+ ' . $key . ': ' . var_export($value, true) . "\n";
+    }
+    );
     var_dump($difference);
 }

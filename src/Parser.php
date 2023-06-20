f17fe7099fa49e865275c8000514c0bddd5fac58
VoronczovaPolina <tirabyrius@gmail.com>
2023-06-20 13:57:32 +0500

    fix yml parser error

--- src/Parser.php
+++ src/Parser.php
@@ -1,6 +1,8 @@
 <?php 
 namespace Differ\Parser;
 
+use Symfony\Component\Yaml\Yaml;
+
 function parser($pathFile1, $pathFile2)
 {
     $file1 = file_get_contents($pathFile1);
@@ -8,8 +10,7 @@ function parser($pathFile1, $pathFile2)
     if ((pathinfo($pathFile1, PATHINFO_EXTENSION) === 'json') && ((pathinfo($pathFile2, PATHINFO_EXTENSION) === 'json'))) {
         $array1 = json_decode($file1, true);
         $array2 = json_decode($file2, true);
-    } elseif ((pathinfo($pathFile1, PATHINFO_EXTENSION) === 'yml' || pathinfo($pathFile1, PATHINFO_EXTENSION) === 'yaml') && (pathinfo($pathFile2, PATHINFO_EXTENSION) === 'yml' || pathinfo($pathFile2, PATHINFO_EXTENSION) === 'yaml'))
-    {
+    } elseif ((pathinfo($pathFile1, PATHINFO_EXTENSION) === 'yml' || pathinfo($pathFile1, PATHINFO_EXTENSION) === 'yaml') && (pathinfo($pathFile2, PATHINFO_EXTENSION) === 'yml' || pathinfo($pathFile2, PATHINFO_EXTENSION) === 'yaml')) {
         $array1 = Yaml::parse($file1);
         $array2 = Yaml::parse($file2);
     }

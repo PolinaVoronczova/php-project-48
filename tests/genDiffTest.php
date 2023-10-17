f8aafec7238a43c57486779075c9beb38321b567
VoronczovaPolina <tirabyrius@gmail.com>
2023-10-17 17:58:58 +0500

    fix error

--- tests/genDiffTest.php
+++ tests/genDiffTest.php
@@ -5,8 +5,6 @@ namespace Differ\Phpunit\Tests;
 use PHPUnit\Framework\TestCase;
 use function Differ\GenDiff\genDiff;
 
-$filePath1 = 'tests/fixtures/file1.json';
-$filePath2 = 'tests/fixtures/file2.json';
 class genDiffTest extends TestCase
 {
     public function testGenDiff(): void
@@ -15,63 +13,55 @@ class genDiffTest extends TestCase
         $fileYmlPath2 = 'tests/fixtures/file2.yml';
         $expectedResult1 = "  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n";
         $this->assertEquals($expectedResult1, genDiff($fileYmlPath1, $fileYmlPath2, 'stylish'));
-    }
 
-    public function testGetStylishFormated(): void
-    {
         $filePath1 = 'tests/fixtures/file1.json';
         $filePath2 = 'tests/fixtures/file2.json';
         $expectedResult2 = "{
-            common: {
-              + follow: false
-                setting1: Value 1
-              - setting2: 200
-              - setting3: true
-              + setting3: null
-              + setting4: blah blah
-              + setting5: {
-                    key5: value5
-                }
-                setting6: {
-                    doge: {
-                      - wow:
-                      + wow: so much
-                    }
-                    key: value
-                  + ops: vops
-                }
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
             }
-            group1: {
-              - baz: bas
-              + baz: bars
-                foo: bar
-              - nest: {
-                    key: value
-                }
-              + nest: str
-            }
-          - group2: {
-                abc: 12345
-                deep: {
-                    id: 45
-                }
-            }
-          + group3: {
-                deep: {
-                    id: {
-                        number: 45
-                    }
-                }
-                fee: 100500
-            }
-        }";
-        $this->assertEquals($expectedResult2, genDiff($filePath1, $filePath2, 'stylish'));
+            key: value
+          + ops: vops
+        }
     }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}";
+        $this->assertEquals($expectedResult2, genDiff($filePath1, $filePath2, 'stylish'));
 
-    public function testGetPlainFormated(): void
-    {
-        $filePath1 = 'tests/fixtures/file1.json';
-        $filePath2 = 'tests/fixtures/file2.json';
         $expectedResult3 = "Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
@@ -85,128 +75,144 @@ Property 'group2' was removed
 Property 'group3' was added with value: [complex value]
 ";
         $this->assertEquals($expectedResult3, genDiff($filePath1, $filePath2, 'plain'));
-    }
 
-    public function testGetJsonFormated(): void
+      $expectedResult4 = '[
     {
-        $filePath1 = 'tests/fixtures/file1.json';
-        $filePath2 = 'tests/fixtures/file2.json';
-        $expectedResult4 = '[
+        "key": "common",
+        "status": "array",
+        "children": [
             {
-                "key": "common",
-                "status": "array",
+                "key": "follow",
+                "status": "was added",
+                "value": false
+            },
+            {
+                "key": "setting1",
+                "status": "no change",
+                "value": "Value 1"
+            },
+            {
+                "key": "setting2",
+                "status": "was removed",
+                "removed value": 200
+            },
+            {
+                "key": "setting3",
+                "status": "was updated",
+                "old value": true,
+                "new value": null
+            },
+            {
+                "key": "setting4",
+                "status": "was added",
+                "value": "blah blah"
+            },
+            {
+                "key": "setting5",
+                "status": "was added array",
                 "children": [
                     {
-                        "key": "follow",
-                        "status": "was added",
-                        "value": false
-                    },
-                    {
-                        "key": "setting1",
+                        "key": "key5",
                         "status": "no change",
-                        "value": "Value 1"
-                    },
-                    {
-                        "key": "setting2",
-                        "status": "was removed",
-                        "removed value": 200
-                    },
-                    {
-                        "key": "setting3",
-                        "status": "was updated",
-                        "old value": true,
-                        "new value": null
-                    },
-                    {
-                        "key": "setting4",
-                        "status": "was added",
-                        "value": "blah blah"
-                    },
-                    {
-                        "key": "setting5",
-                        "status": "was added array",
-                        "children": [
-                            {
-                                "key": "key5",
-                                "status": "no change",
-                                "value": "value5"
-                            }
-                        ]
-                    },
-                    {
-                        "key": "setting6",
-                        "status": "array",
-                        "children": [
-                            {
-                                "key": "doge",
-                                "status": "array",
-                                "children": [
-                                    {
-                                        "key": "wow",
-                                        "status": "was updated",
-                                        "old value": "",
-                                        "new value": "so much"
-                                    }
-                                ]
-                            },
-                            {
-                                "key": "key",
-                                "status": "no change",
-                                "value": "value"
-                            },
-                            {
-                                "key": "ops",
-                                "status": "was added",
-                                "value": "vops"
-                            }
-                        ]
+                        "value": "value5"
                     }
                 ]
             },
             {
-                "key": "group1",
+                "key": "setting6",
                 "status": "array",
                 "children": [
                     {
-                        "key": "baz",
-                        "status": "was updated",
-                        "old value": "bas",
-                        "new value": "bars"
-                    },
-                    {
-                        "key": "foo",
-                        "status": "no change",
-                        "value": "bar"
-                    },
-                    {
-                        "key": "nest",
-                        "status": "was updated array",
-                        "old value": [
-                            {
-                                "key": "key",
-                                "status": "no change",
-                                "value": "value"
-                            }
-                        ],
-                        "new value": "str"
-                    }
-                ]
-            },
-            {
-                "key": "group2",
-                "status": "was removed array",
-                "children": [
-                    {
-                        "key": "abc",
-                        "status": "no change",
-                        "value": 12345
-                    },
-                    {
-                        "key": "deep",
+                        "key": "doge",
                         "status": "array",
                         "children": [
                             {
-                                "key": "id",
+                                "key": "wow",
+                                "status": "was updated",
+                                "old value": "",
+                                "new value": "so much"
+                            }
+                        ]
+                    },
+                    {
+                        "key": "key",
+                        "status": "no change",
+                        "value": "value"
+                    },
+                    {
+                        "key": "ops",
+                        "status": "was added",
+                        "value": "vops"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "key": "group1",
+        "status": "array",
+        "children": [
+            {
+                "key": "baz",
+                "status": "was updated",
+                "old value": "bas",
+                "new value": "bars"
+            },
+            {
+                "key": "foo",
+                "status": "no change",
+                "value": "bar"
+            },
+            {
+                "key": "nest",
+                "status": "was updated array",
+                "old value": [
+                    {
+                        "key": "key",
+                        "status": "no change",
+                        "value": "value"
+                    }
+                ],
+                "new value": "str"
+            }
+        ]
+    },
+    {
+        "key": "group2",
+        "status": "was removed array",
+        "children": [
+            {
+                "key": "abc",
+                "status": "no change",
+                "value": 12345
+            },
+            {
+                "key": "deep",
+                "status": "array",
+                "children": [
+                    {
+                        "key": "id",
+                        "status": "no change",
+                        "value": 45
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "key": "group3",
+        "status": "was added array",
+        "children": [
+            {
+                "key": "deep",
+                "status": "array",
+                "children": [
+                    {
+                        "key": "id",
+                        "status": "array",
+                        "children": [
+                            {
+                                "key": "number",
                                 "status": "no change",
                                 "value": 45
                             }
@@ -215,34 +221,13 @@ Property 'group3' was added with value: [complex value]
                 ]
             },
             {
-                "key": "group3",
-                "status": "was added array",
-                "children": [
-                    {
-                        "key": "deep",
-                        "status": "array",
-                        "children": [
-                            {
-                                "key": "id",
-                                "status": "array",
-                                "children": [
-                                    {
-                                        "key": "number",
-                                        "status": "no change",
-                                        "value": 45
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        "key": "fee",
-                        "status": "no change",
-                        "value": 100500
-                    }
-                ]
+                "key": "fee",
+                "status": "no change",
+                "value": 100500
             }
-        ]';
-        $this->assertEquals($expectedResult4, genDiff($filePath1, $filePath2, 'json'));
+        ]
+    }
+]';
+            $this->assertEquals($expectedResult4, genDiff($filePath1, $filePath2, 'json'));
     }
 }

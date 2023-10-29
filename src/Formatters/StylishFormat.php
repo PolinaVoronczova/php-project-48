fcb192965ff3e72f3ccd8e2a4c033572512fa8d9
VoronczovaPolina <tirabyrius@gmail.com>
2023-10-29 20:00:05 +0500

    fix error

--- src/Formatters/StylishFormat.php
+++ src/Formatters/StylishFormat.php
@@ -14,74 +14,81 @@ function iter(array $buildDiff, int $depth = 1)
 {
     $result = array_reduce(
         $buildDiff,
-        function ($resultt, $item) use ($depth) {
+        function ($result, $item) use ($depth) {
             $key = $item['key'];
             $status = $item['status'];
-            $result = $resultt;
             switch ($status) {
                 case 'array':
                     $children = $item['children'];
                     $itemChildren = implode("\n", iter($children, $depth + 1));
-                    $result[] = str_repeat(" ", $depth * 4) . $key . ": {\n"
+                    $resultAdd = str_repeat(" ", $depth * 4) . $key . ": {\n"
                     . $itemChildren . "\n" . str_repeat(" ", $depth * 4) . "}";
+                    return array_merge($result, [$resultAdd]);
                     break;
                 case 'no change':
                     $value = $item['value'];
-                    $result[] = str_repeat(" ", $depth * 4)
+                    $resultAdd = str_repeat(" ", $depth * 4)
                     . $key . ": " . getString($value);
+                    return array_merge($result, [$resultAdd]);
                     break;
                 case 'update':
                     $oldValue = $item["oldValue"];
                     $newValue = $item["newValue"];
-                    $result[] = str_repeat(" ", $depth * 4 - 2)
-                    . "- " . $key . ": " . getString($oldValue);
-                    $result[] = str_repeat(" ", $depth * 4 - 2)
+                    $resultAdd = str_repeat(" ", $depth * 4 - 2)
+                    . "- " . $key . ": " . getString($oldValue). "\n"
+                    . str_repeat(" ", $depth * 4 - 2)
                     . "+ " . $key . ": " . getString($newValue);
+                    return array_merge($result, [$resultAdd]);
                     break;
                 case 'add':
                     $value = $item['value'];
-                    $result[] = str_repeat(" ", $depth * 4 - 2)
+                    $resultAdd = str_repeat(" ", $depth * 4 - 2)
                     . "+ " . $key . ": " . getString($value);
+                    return array_merge($result, [$resultAdd]);
                     break;
                 case 'delete':
                     $value = $item['value'];
-                    $result[] = str_repeat(" ", $depth * 4 - 2)
+                    $resultAdd = str_repeat(" ", $depth * 4 - 2)
                     . "- " . $key . ": " . getString($value);
+                    return array_merge($result, [$resultAdd]);
                     break;
                 case 'add array':
                     $children = $item['children'];
                     $itemChildren = implode("\n", getStylishArray($children, $depth + 1));
-                    $result[] = str_repeat(" ", $depth * 4 - 2)
+                    $resultAdd = str_repeat(" ", $depth * 4 - 2)
                     . "+ " . $key . ": {\n" . $itemChildren . "\n" . str_repeat(" ", $depth * 4) . "}";
+                    return array_merge($result, [$resultAdd]);
                     break;
                 case 'delete array':
                     $children = $item['children'];
                     $itemChildren = implode("\n", getStylishArray($children, $depth + 1));
-                    $result[] = str_repeat(" ", $depth * 4 - 2)
+                    $resultAdd = str_repeat(" ", $depth * 4 - 2)
                     . "- " . $key . ": {\n" . $itemChildren . "\n" . str_repeat(" ", $depth * 4) . "}";
+                    return array_merge($result, [$resultAdd]);
                     break;
                 case 'update array':
+                    $updateResult = [];
                     if (is_array($item["oldValue"])) {
                         $oldValue = implode("\n", getStylishArray($item["oldValue"], $depth + 1));
-                        $result[] = str_repeat(" ", $depth * 4 - 2)
+                        $updateResult[] = str_repeat(" ", $depth * 4 - 2)
                         . "- " . $key . ": {\n" . $oldValue . "\n" . str_repeat(" ", $depth * 4) . "}";
                     } else {
                         $oldValue = $item["oldValue"];
-                        $result[] = str_repeat(" ", $depth * 4 - 2)
+                        $updateResult[] = str_repeat(" ", $depth * 4 - 2)
                         . "- " . $key . ": " . getString($oldValue);
                     }
                     if (is_array($item["newValue"])) {
                         $newValue = implode("\n", getStylishArray($item["newValue"], $depth + 1));
-                        $result[] = str_repeat(" ", $depth * 4 - 2)
+                        $updateResult[] = str_repeat(" ", $depth * 4 - 2)
                         . "+ " . $key . ": {\n" . $newValue . "\n" . str_repeat(" ", $depth * 4) . "}";
                     } else {
                         $newValue = $item["newValue"];
-                        $result[] = str_repeat(" ", $depth * 4 - 2)
+                        $updateResult[] = str_repeat(" ", $depth * 4 - 2)
                         . "+ " . $key . ": " . getString($newValue);
                     }
+                    return array_merge($result, $updateResult);
                     break;
             }
-            return $result;
         },
         []
     );

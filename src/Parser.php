ee1d56b9fe4003d5cad9f3870ef8fbe0e4b71525
VoronczovaPolina <tirabyrius@gmail.com>
2023-11-08 23:10:12 +0500

    add Exception

--- src/Parser.php
+++ src/Parser.php
@@ -4,24 +4,17 @@ namespace Differ\Parser;
 
 use Symfony\Component\Yaml\Yaml;
 
-function parse(string $pathFile1, string $pathFile2)
+function parse(string $pathFile)
 {
-    $file1 = file_get_contents($pathFile1);
-    $file2 = file_get_contents($pathFile2);
-    if ($file1 === false && $file2 === false) {
-        return [0,0];
-    } elseif ($file1 === false) {
-        return [0,1];
-    } elseif ($file2 === false) {
-        return [1,0];
+    $file = file_get_contents($pathFile);
+    if ($file === false) {
+        return throw new \Exception("File {$pathFile} no exist.");
     }
-    $pathInfoFile1 = pathinfo($pathFile1, PATHINFO_EXTENSION);
-    $pathInfoFile2 = pathinfo($pathFile2, PATHINFO_EXTENSION);
-    $isYml1 = $pathInfoFile1 === 'yml' || $pathInfoFile1 === 'yaml';
-    $isYml2 = $pathInfoFile2 === 'yml' || $pathInfoFile2 === 'yaml';
-    if (($pathInfoFile1 === 'json') && ($pathInfoFile2 === 'json')) {
-        return [json_decode($file1, true), json_decode($file2, true)];
-    } elseif ($isYml1 && $isYml2) {
-        return [Yaml::parse($file1), Yaml::parse($file2)];
+    $pathInfoFile = pathinfo($pathFile, PATHINFO_EXTENSION);
+    $isYml = $pathInfoFile === 'yml';
+    if (($pathInfoFile === 'json')) {
+        return json_decode($file, true);
+    } elseif ($isYml) {
+        return Yaml::parse($file);
     }
 }
